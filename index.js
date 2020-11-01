7570d12aeb22b3f26015bed87437ad026f89e4bf
songhee <thdgml5537@gmail.com>
2020-10-31 21:37:27 -0400

    with user inputs, create new README file as readmeFrame format"

--- index.js
+++ index.js
@@ -2,10 +2,30 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const readmeFrame = require('./readmeFrame.js');
 
+
 const userPrompt = inquirer.prompt([
     {
         type: 'input',
         name: 'title',
         message: 'What is the README title?'
-    }
-])
+    },
+    {
+        type:'input',
+        name: 'description',
+        message: "Add your description: ",
+        choices: ['Installation', 'URLs', 'Usage', 'Credits', 'License']
+    },
+    {
+        type: 'input',
+        name: 'url',
+        message:'Enter your github ID'
+    },
+]);
+
+userPrompt
+.then(response => readmeFrame(response))
+.then(result => fs.writeFile('README.md', result, 'utf-8', err => {
+    if(err) throw err;
+}));
+
+
