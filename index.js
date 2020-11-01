9a5f94348182bc45e19566f47a3e699ddbf80e7f
songhee <thdgml5537@gmail.com>
2020-10-31 23:49:11 -0400

    add license array in seperate file and like to index.js file

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const readmeFrame = require('./readmeFrame.js');
 const util = require('util');
+const licenseArray = require('./license.js');
 
 const userPrompt = inquirer.prompt([
+    {
+        type:'input',
+        name:'name',
+        message: "What is your name?"
+    },
     {
         type: 'input',
         name: 'title',
@@ -12,21 +18,36 @@ const userPrompt = inquirer.prompt([
     {
         type:'input',
         name: 'description',
-        message: "Add your description: ",
-        choices: ['Installation', 'URLs', 'Usage', 'Credits', 'License']
+        message: "Add your description: "
     },
     {
         type: 'input',
         name: 'url',
-        message:'Enter your github ID'
+        message:'Enter your github ID: '
     },
+    {
+        type: 'list',
+        name: 'license',
+        message: "Select license type: ",
+        choices: ["Apache License", "ISC License", "MIT License", "Public License"]
+    }
 ]);
 
 const writeFileAsync = util.promisify(fs.writeFile);
+const addLicenseAsync = util.promisify(fs.appendFile);
 
 const generateReadme = async () =>{
     try{
-        const userInput = await userPrompt;
+        let userInput = await userPrompt;
+            if (userInput.license == 'Apache License'){
+                userInput.license = licenseArray[0];
+            }else if(userInput.license == 'ISC License'){
+                userInput.license = licenseArray[1];
+            }else if(userInput.license == 'MIT License'){
+                userInput.license = licenseArray[2];
+            }else{
+                userInput.license =  licenseArray[3];
+            }
         const frameWork = readmeFrame(userInput);
         const final = await writeFileAsync('README.md', frameWork);
         return final;
